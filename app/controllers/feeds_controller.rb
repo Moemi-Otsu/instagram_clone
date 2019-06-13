8629a75fc14af0e880f4a1f31a94b6f7fbd5d24a
moemi ohtsu <s0623192@gmail.com>
2019-06-13 16:47:44 +0900

    Feat: Feedの画像アップロード機能途中まで

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,9 +1,26 @@
 class FeedsController < ApplicationController
-  
+
   def index
+    @feeds = Feed.all
   end
 
   def new
+    @feed = Feed.new
+  end
+
+  def create
+    @feed = Feed.new(feed_params)
+    if @feed.save
+    redirect_to feeds_path, notice: "投稿しました"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:image, :image_cache)
   end
 
 end
