3082f344ffe7abc0aa8db3c6af9504dee7599b39
moemi ohtsu <s0623192@gmail.com>
2019-06-14 12:32:52 +0900

    Feat: has_secure_passwordメソッド用のbcryptのGemを追加してBundleInstall

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'carrierwave'
 gem 'mini_magick'
 
+# has_secure_passwordメソッドを使用するためのbcrypt
+gem 'bcrypt', '3.1.11'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  # begore_validationコールバック。保存する前にメールアドレスの値を小文字に変換する。
+  # before_validationコールバック。保存する前にメールアドレスの値を小文字に変換する。
   before_validation { email.downcase! }
+  # has_secure_passwordメソッド:セキュアにハッシュ化したパスワードを、データベース内のpassword_digestというカラムに保存する
+  has_secure_password
 end
