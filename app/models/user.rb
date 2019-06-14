a540be96430114598c08e0d6df349c7a8ea87373
moemi ohtsu <s0623192@gmail.com>
2019-06-14 12:20:48 +0900

    Feat: before_validationコールバック設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  # begore_validationコールバック。保存する前にメールアドレスの値を小文字に変換する。
+  before_validation { email.downcase! }
 end
