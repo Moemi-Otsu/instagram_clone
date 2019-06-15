f3e119cc229148cb5445afcb98f9404eac8716e7
moemi ohtsu <s0623192@gmail.com>
2019-06-15 17:53:19 +0900

    Feat: いいね機能の実装

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,12 @@
+class LikesController < ApplicationController
+  def create
+    like = current_user.likes.create(feed_id: params[:feed_id])
+    redirect_to feeds_url, notice: "#{like.feed.user.name}さんの投稿をLikeしました"
+  end
+
+  def destroy
+    like = current_user.likes.find_by(id: params[:id]).destroy
+    redirect_to feeds_url, notice: "#{like.feed.user.name}さんの投稿をLike解除しました"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'likes/new'
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   # resources :users, only: [:new, :create, :show]
