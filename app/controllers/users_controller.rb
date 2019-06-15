88e0070c0fea2feb6f3833ac34b8adba16743ca2
moemi ohtsu <s0623192@gmail.com>
2019-06-15 20:44:36 +0900

    Feat: ユーザープロフィール画像アップロード機能共通処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -13,15 +15,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    #before_actionに設定
   end
 
   def edit
-    @user = User.find(params[:id])
+    #before_actionに設定
   end
 
   def update
-    @user = User.find(params[:id])
+    #before_actionに設定
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: "プロフィールを編集しました"
     else
@@ -33,6 +35,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password, :password_confirmation, :image, :image_cache)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
