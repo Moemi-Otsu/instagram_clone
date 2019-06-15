950b7400a622f01c12b910f684170fcc1fcd0a2a
moemi ohtsu <s0623192@gmail.com>
2019-06-15 19:08:52 +0900

    Feat: usersテーブルにimage（text型)カラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Feedモデルのimageカラムに、ImageUploaderを紐付けます。
+  mount_uploader :image, ImageUploader
+  # バリデーション
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   # before_validationコールバック。保存する前にメールアドレスの値を小文字に変換する。
