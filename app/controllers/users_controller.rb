20fe942626272af874c69857194c199e96df23b6
moemi ohtsu <s0623192@gmail.com>
2019-06-21 11:37:58 +0900

    Fix: herokuで挙動がおかしいためusers_controller.rbの22行目session記述をコメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     #end
 
     if @user.save
-      session[:user_id] = @user.id
+      　#session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render 'new'
